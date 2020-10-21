89cd7606c7e5a94f358a24ff5fe1af29bfbfa183
Alex Van Boxel <alex@vanboxel.be>
2020-10-21 13:39:39 +0200

    Add Google Pubsub as an OTLP exporter/receiver

--- cmd/otelcontribcol/components.go
+++ cmd/otelcontribcol/components.go
@@ -30,6 +30,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/elasticexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/f5cloudexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/googlecloudexporter"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/googlecloudpubsubexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/honeycombexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/humioexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/influxdbexporter"
@@ -69,6 +70,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/dotnetdiagnosticsreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/fluentforwardreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/googlecloudpubsubreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/influxdbreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/jmxreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver"
@@ -139,6 +141,7 @@ func components() (component.Factories, error) {
 		wavefrontreceiver.NewFactory(),
 		windowsperfcountersreceiver.NewFactory(),
 		zookeeperreceiver.NewFactory(),
+		googlecloudpubsubreceiver.NewFactory(),
 		syslogreceiver.NewFactory(),
 		tcplogreceiver.NewFactory(),
 		udplogreceiver.NewFactory(),
@@ -167,6 +170,7 @@ func components() (component.Factories, error) {
 		elasticexporter.NewFactory(),
 		f5cloudexporter.NewFactory(),
 		googlecloudexporter.NewFactory(),
+		googlecloudpubsubexporter.NewFactory(),
 		honeycombexporter.NewFactory(),
 		humioexporter.NewFactory(),
 		influxdbexporter.NewFactory(),
--- exporter/googlecloudpubsubexporter/go.mod
+++ exporter/googlecloudpubsubexporter/go.mod
@@ -3,8 +3,13 @@ module github.com/open-telemetry/opentelemetry-collector-contrib/exporter/google
 go 1.16
 
 require (
+	cloud.google.com/go/pubsub v1.15.0
+	github.com/google/uuid v1.3.0
 	github.com/stretchr/testify v1.7.0
 	go.opentelemetry.io/collector v0.33.0
 	go.opentelemetry.io/collector/model v0.33.0
 	go.uber.org/zap v1.19.0
+	google.golang.org/api v0.54.0
+	google.golang.org/genproto v0.0.0-20210813162853-db860fec028c
+	google.golang.org/grpc v1.40.0
 )
